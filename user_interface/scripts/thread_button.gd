331391a7f32b78280a5f9beedb39efa94949c867
Sparrow <anna.k.harsanyi@gmail.com>
2025-09-09 22:08:53 +0200

    Slightly refactored ThreadButton and ThreadButtonContainer

--- user_interface/scripts/thread_button.gd
+++ user_interface/scripts/thread_button.gd
@@ -2,10 +2,8 @@
 class_name ThreadButton
 extends Button
 
-var thread: XStitchThread:
-	set(value):
-		thread = value
-		_refresh()
+var thread: XStitchThread
+var icon_color: Color
 
 var styleboxes = [
 	"hover",
@@ -22,26 +20,35 @@ var theme_color_overrides = [
 	"icon_pressed_color"
 ]
 
+## Set the context menu to the right mouse button click.
 func set_context_menu(context_menu: Node) -> void:
-	find_child("RMB").context_menu = context_menu
+	$RMB.context_menu = context_menu
 
-func _refresh() -> void:
+## Assign a thread to this button.
+func assign_thread(_thread: XStitchThread) -> void:
+	thread = _thread
 	tooltip_text = thread.color_name
-	
+	icon_color = get_icon_color()
+	set_theme_overrides()
+
+## Calculate the icon luminance based on the background color.
+## Produces a black or white icon
+func get_icon_color() -> Color:
+	var luminance = thread.color.get_luminance()
+	if luminance > 0.5:
+		return Color.BLACK
+	else:
+		return Color.WHITE
+
+func set_theme_overrides():
 	for setting in styleboxes:
 		var stylebox := get_theme_stylebox(setting).duplicate()
 		stylebox.bg_color = thread.color
 		add_theme_stylebox_override(setting, stylebox)
 	
-	var icon_color : Color
-	var luminance = thread.color.get_luminance()
-	if luminance > 0.5:
-		icon_color = Color.BLACK
-	else:
-		icon_color = Color.WHITE
-	
 	for setting in theme_color_overrides:
 		add_theme_color_override(setting, icon_color)
+
 
 func _on_pressed() -> void:
 	SignalBus.thread_button_clicked.emit(thread, self, get_parent())
