d7dab2186f768bc41d8b3fa3756e1b6bdafb4b69
Sparrow <anna.k.harsanyi@gmail.com>
2025-09-10 13:46:36 +0200

    Renamed Symbol to XStitchSymbol

--- scripts/cross_stitch_symbol.gd
+++ scripts/cross_stitch_symbol.gd
@@ -1,9 +1,10 @@
-class_name Symbol
+class_name XStitchSymbol
 extends Resource
 
 @export var id : String
 @export var symbol_name : String
 @export var data : CompressedTexture2D
+var assigned := false
 
 func get_identifying_name() -> String :
 	return id;
