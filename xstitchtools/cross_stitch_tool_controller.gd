8b778f4d7793023a0ae6dcff5c64f9740cf2dcec
Sparrow <anna.k.harsanyi@gmail.com>
2025-09-14 21:37:45 +0200

    Fixed: Tool bar jitter when Draw & Erase settings are hidden. Fixed: Better tool UI control.

--- xstitchtools/cross_stitch_tool_controller.gd
+++ xstitchtools/cross_stitch_tool_controller.gd
@@ -15,13 +15,16 @@ func get_current_tool() -> XStitchTool:
 	return current_tool
 
 func select_tool(method: XStitchTool.Method):
-	if current_tool && tool_ui_dictionary[current_tool.method]:
-		tool_ui_dictionary[current_tool.method].hide()
+	if current_tool:
+		set_tool_ui_visibility(current_tool.method, false)
 	
 	current_tool = tool_dictionary[method]
 	
-	if tool_ui_dictionary[current_tool.method]:
-		tool_ui_dictionary[current_tool.method].show()
+	set_tool_ui_visibility(method, true)
 	
 	Input.set_custom_mouse_cursor(current_tool.cursor_image, 0, current_tool.cursor_hotspot)
 	tool_selected.emit(current_tool)
+
+func set_tool_ui_visibility(idx: int, visible: bool):
+	if idx < tool_ui_dictionary.size():
+		tool_ui_dictionary[idx].visible = visible
